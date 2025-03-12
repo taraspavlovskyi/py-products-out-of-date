21600cfb18f463bc61351fa2dc0680742b2b2073
Taras Pavlovskyi <taras.pavlovskyi@gmail.com>
2025-03-12 15:19:28 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,17 @@
-# write your code here
+import datetime
+from collections.abc import Callable
+import pytest
+from unittest import mock
+
+
+@pytest.fixture()
+def mocked_datetime_date_today() -> None:
+    with (mock.patch("datetime.date.today", return_value=datetime.date(
+            2022, 2, 5)) as mock_test_datetime):
+        yield mock_test_datetime
+
+
+def test_datetime_date_today(mocked_datetime_date_today: Callable) -> None:
+    today = datetime.date.today()
+    mocked_datetime_date_today.assert_called_once()
+    assert today == datetime.date(2022, 2, 5)
